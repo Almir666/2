f9173c2e3bcb6a6a7a16a54d0ad3eba5a50c7890
Almir <crafteasy@yandex.ru>
2022-07-22 18:09:08 +0300

    Создал ввод и вывод массива

--- Program.cs
+++ Program.cs
@@ -2,4 +2,25 @@
 // которыъ меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма.  
 
-
+Console.WriteLine("Введите колличество элементов массива");
+int CollichestvoStrok = int.Parse(Console.ReadLine());
+string[] MassivStrok = new string[CollichestvoStrok];
+string[] FillArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите" + (count + 1) "элемент из символов" + ":");
+        arr[i] = Console.ReadLine();
+    }
+}
+string[] PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            Console.Write($" {arr[i]} ");
+        }
+    }
+}
