8f0c96e4a7d1e35e32ca41feb6652215bdef1ce4
Almir <crafteasy@yandex.ru>
2022-07-22 20:26:12 +0300

    создал новую ветку

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // которыъ меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма.  
 
-Console.WriteLine("Введите колличество элементов массива:");
+Console.WriteLine("Привет дружок! Введи колличество элементов массива :* ");
 int CollichestvoStrok = int.Parse(Console.ReadLine());
 string[] MassivStrok = new string[CollichestvoStrok];
 void FillArray(string[] arr)
@@ -10,7 +10,7 @@ void FillArray(string[] arr)
     int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.WriteLine("Введите " + (count + 1) + " элемент из символов" + ":");
+        Console.WriteLine("Введи " + (count + 1) + " элемент из символов" + ":");
         arr[i] = Console.ReadLine();
         count++;
     }
