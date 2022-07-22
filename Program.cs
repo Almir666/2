ff2b009e0ca5b357debf044e349d6992318cf240
Almir <crafteasy@yandex.ru>
2022-07-22 20:50:37 +0300

    Решил задачу через изначально заданный массив

--- Program.cs
+++ Program.cs
@@ -2,19 +2,8 @@
 // которыъ меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма.  
 
-Console.WriteLine("Привет дружок! Введи колличество элементов массива :* ");
-int CollichestvoStrok = int.Parse(Console.ReadLine());
-string[] MassivStrok = new string[CollichestvoStrok];
-void FillArray(string[] arr)
-{
-    int count = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.WriteLine("Введи " + (count + 1) + " элемент из символов" + ":")
-        arr[i] = Console.ReadLine();
-        count++;
-    }
-}
+string[] MassivStrok = {"Hello", "cat", ":-)", "World"};
+
 void PrintArray(string[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
@@ -26,7 +15,6 @@ void PrintArray(string[] arr)
     }
 }
 
-FillArray(MassivStrok);
 Console.WriteLine();
 Console.WriteLine("Сформирован массив из подходящих строк:");
 PrintArray(MassivStrok);
