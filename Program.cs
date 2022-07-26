766347c8ee69480a40834f3b683f287f68289e8b
Almir <crafteasy@yandex.ru>
2022-07-26 22:43:58 +0300

    Получилось

--- Program.cs
+++ Program.cs
@@ -3,25 +3,25 @@
 // либо задать на старте выполнения алгоритма.  
 
 
-string[] StrLength3(string[] arrayOld)
+string[] StrLength3(string[] arrayOld, string value)
 {
-    string[] arrayNew = new string[arrayOld.Length];
-    for(int i = 0; i < arrayOld.Length; i++)
+    string[] arrayNew = new string[arrayOld.Length + 1];
+    for (int i = 0; i < arrayOld.Length; i++)
     {
         arrayNew[i] = arrayOld[i];
     }
+    arrayNew[arrayNew.Length - 1] = value;
     return arrayNew;
 }
 
-int letter = 3;
-string[] data = new string[] {"Hello", "hi", ":)", "cat", "World"};
+string[] array = new string[] { "Hello", "hi", ":)", "cat", "World" };
 string[] result = new string[0];
-for(int i = 0; i < data.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    int strLength = data[i].Length;
-    if(strLength <= letter)
+    int strLength = array[i].Length;
+    if (strLength <= 3)
     {
-        result = StrLength3(data);
+        result = StrLength3(result, array[i]);
     }
 }
 
