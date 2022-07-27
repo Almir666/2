658a4fcfac90a3cc785102fa3fa2a97a0d46e9b5
Almir <crafteasy@yandex.ru>
2022-07-27 15:30:28 +0300

    Готовое решение

--- Program.cs
+++ Program.cs
@@ -3,26 +3,43 @@
 // либо задать на старте выполнения алгоритма.  
 
 
-string[] StrLength3(string[] arrayOld, string value)
+string[] arrayOld = new string[] { "fwer", "cat", "gerg", "fsfg", "ff" };
+
+int ArrayFindWords3(string[] arr)
 {
-    string[] arrayNew = new string[arrayOld.Length + 1];
-    for (int i = 0; i < arrayOld.Length; i++)
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
     {
-        arrayNew[i] = arrayOld[i];
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
     }
-    arrayNew[arrayNew.Length - 1] = value;
+    return count;
+}
+
+string[] ArrayComplete(string[] arr, int count)
+{
+    int j = 0;
+    string[] arrayNew = new string[count];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arrayNew[j] = arr[i];
+            j++;
+        }
+    }
     return arrayNew;
 }
 
-string[] array = new string[] { "Hello", "hi", ":)", "cat", "World" };
-string[] result = new string[0];
-for (int i = 0; i < array.Length; i++)
+void PrintArray(string[] arr)
 {
-    int strLength = array[i].Length;
-    if (strLength <= 3)
+    for (int i = 0; i < arr.Length; i++)
     {
-        result = StrLength3(result, array[i]);
+        Console.Write($"{arr[i]}, ");
     }
+    Console.WriteLine();
 }
 
-Console.WriteLine(String.Join(", ", result));
+PrintArray(ArrayComplete(arrayOld, ArrayFindWords3(arrayOld)));
